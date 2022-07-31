d551934001ce1b1b39c3eac7d9e715663875be95
Sangmin <23mean@naver.com>
2022-07-31 15:20:58 +0900

    test: 적절한 게임횟수를 입력 후 '확인'을 클릭하기 까지의 로직을 확인

--- cypress/e2e/racing.cy.js
+++ cypress/e2e/racing.cy.js
@@ -1,6 +1,6 @@
 import { DOM } from '../../lib/constants';
 
-import { carNameInputProcess } from '../support/commands';
+import { carNameInputProcess, gameCountInputProcess } from '../support/commands';
 
 describe('레이싱 게임', () => {
   const baseUrl = '../../index.html';
@@ -36,6 +36,46 @@ describe('레이싱 게임', () => {
       carNameInputProcess(NAME_INPUT_CASE);
 
       cy.get(`#${DOM.GAME_COUNT_CONTAINER_ID}`).should('not.be.visible');
+    });
+  });
+
+  context('게임 횟수 입력을 테스트한다.', () => {
+    const NAME_INPUT_CASE = 'one,two,three,four,five';
+
+    it('게임 횟수를 입력하지 않았을 때, 다음으로 진행되지 않음.', () => {
+      carNameInputProcess(NAME_INPUT_CASE);
+
+      cy.get(`#${DOM.GAME_COUNT_INPUT_ID}`).invoke('val', '');
+      cy.get(`#${DOM.GAME_COUNT_BUTTON_ID}`).click();
+
+      cy.get(`#${DOM.RACER_NAME_CONTAINER_ID}`).should('not.be.visible');
+    });
+
+    it('게임 횟수에 0 이하의 수를 입력했을 때, 다음으로 진행되지 않음.', () => {
+      carNameInputProcess(NAME_INPUT_CASE);
+
+      const COUNT_INPUT_CASE = -1;
+      gameCountInputProcess(COUNT_INPUT_CASE);
+
+      cy.get(`#${DOM.RACER_NAME_CONTAINER_ID}`).should('not.be.visible');
+    });
+
+    it('게임 횟수에 정수가 아닌 수를 입력했을 때,다음으로 진행되지 않음.', () => {
+      carNameInputProcess(NAME_INPUT_CASE);
+
+      const COUNT_INPUT_CASE = 1.5;
+      gameCountInputProcess(COUNT_INPUT_CASE);
+
+      cy.get(`#${DOM.RACER_NAME_CONTAINER_ID}`).should('not.be.visible');
+    });
+
+    it('입력이 완료된 정보는 다시 입력할 수 없음.', () => {
+      carNameInputProcess(NAME_INPUT_CASE);
+
+      const COUNT_INPUT_CASE = 5;
+      gameCountInputProcess(COUNT_INPUT_CASE);
+
+      cy.get(`#${DOM.RACER_NAME_CONTAINER_ID}`).should('not.be.visible');
     });
   });
 });
